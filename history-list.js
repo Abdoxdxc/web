de0c77379d54824ae101bad7e05bf3452f57e5b1
Abdoxdxc <132761572+Abdoxdxc@users.noreply.github.com>
2024-05-21 14:15:23 +0400

    Add files via upload

---
+++ history-list.js
@@ -0,0 +1,44 @@
+let allVideos = [
+    {
+       name: " كيف بدأت الإمارات",
+       src: "https://www.youtube.com/embed/XIg76Z6isXE?si=VmGKldvOl-XcIxNn",
+       id: "vid_1"
+    },
+    {
+       name: "القائدان البطلان",
+       src: "https://www.youtube.com/embed/Aq13c_bQiCg?si=DPRU3i5v_duN7K0V",
+       id: "vid_2"
+    },
+    {
+       name: "ما هو  أقدم أثر على وجود الإنسان في الإمارات؟",
+       src: "https://www.youtube.com/embed/_lkn7yyA_5M?si=QVF4w8Wbf-6U6mmw",
+       id: "vid_3"
+    },
+    {
+       name: "هل تاريخ الامارات بدأ منذ خمسين سنة",
+       src: "https://www.youtube.com/embed/_MorVaS23Q4?si=STrJ7UlbEVEmwveJ",
+       id: "vid_4"
+    },
+    {
+       name: "دولة الامارات العربية المتحدة",
+       src: "https://www.youtube.com/embed/yVjk_wsGEYE?si=wcZUqUtNrpsAR2zY",
+       id: "vid_5"
+    },
+    {
+       name: "الاتحاد بين الامارات السبع",
+       src: "https://www.youtube.com/embed/fwmvjTt3UO4?si=zEzexqMlUCR2QuXM",
+       id: "vid_6"
+    },
+    {
+       name: "الامارات من القدم ",
+       src: "https://www.youtube.com/embed/Xvi6bkUwXRw?si=o5ahbLWrg0tHTxJH",
+       id: "vid_7"
+    },
+    {
+       name: "الامارات في دقيقتين",
+       src: "https://www.youtube.com/embed/2seRvV-af38?si=tKFQn79Vie10m5PG",
+       id: "vid_8"
+    },
+    
+       
+ ]
